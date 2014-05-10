811c3b390a648ce8c34e692c089165e026a8c832
Suntail <suntail@yandex.ru>
2014-05-10 12:00:31 -0700

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,5 +1,6 @@
 # Read data
 data <- read.table("household_power_consumption.txt", header = TRUE, sep  =";", skip = 66636, nrows = 2880)
+# "GAP" = Global_active_power; "GRP" = Global_reactive_power; "GI" = Global_intensity; "SM1-3" = Sub_metering_1-3
 names(data) <- c("Date", "Time", "GAP", "GRP", "Voltage", "GI", "SM1", "SM2", "SM3")
 # Create plot from Sub_metering_1
 plot(data$SM1, main = "", xlab = "", xaxt = "n", ylab = "Energy sub metering", type = 'l')
