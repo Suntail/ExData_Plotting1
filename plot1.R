b0ff9b4b9a12e40d212336bc45bb9f7d97268656
Suntail <suntail@yandex.ru>
2014-05-10 11:52:47 -0700

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,5 +1,8 @@
+# Read data
 data <- read.table("household_power_consumption.txt", header = TRUE, sep  =";", skip = 66636, nrows = 2880)
 names(data) <- c("Date", "Time", "GAP", "GRP", "Voltage", "GI", "SM1", "SM2", "SM3")
+# Create histograme
 hist(data$GAP, main = "Global Active Power", xlab = "Global Active Power (kilowatts)", col = "red")
+# Save histogram as png file
 dev.copy(png, file = "plot1.png")
 dev.off()
