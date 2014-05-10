0ed2e1768b465b2d51e54f7685ee8a8eb9268508
Suntail <suntail@yandex.ru>
2014-05-10 11:55:04 -0700

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,6 +1,10 @@
+# Read data
 data <- read.table("household_power_consumption.txt", header = TRUE, sep  =";", skip = 66636, nrows = 2880)
 names(data) <- c("Date", "Time", "GAP", "GRP", "Voltage", "GI", "SM1", "SM2", "SM3")
+# Create plot
 plot(data$GAP, main = "", xlab = "", xaxt = "n", ylab = "Global Active Power (kilowatts)", type = 'l')
+# Change x axis labels
 axis(1, at=c(0, 1441, 2880), labels=c("Tue","Fri", "Sat"))
+# Save plot as png file
 dev.copy(png, file = "plot2.png")
 dev.off()
